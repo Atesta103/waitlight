91374749ffa6054e1331a71765d89e6c9be029a9
atesta103 <alexandre.testa.at@gmail.com>
2026-06-17 09:41:55 +0200

    fix: supprimer enableSystem=false, garder defaultTheme=light

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,6 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
-          enableSystem={false}
           disableTransitionOnChange
         >
           {children}
